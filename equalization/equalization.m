4150d397e31ff6efc08f979953a4ece142f4a916
Shengyw1711 <83723115+Shengyw1711@users.noreply.github.com>
2022-02-15 14:55:46 +0800

    Delete equalization directory

--- equalization/equalization.m
+++
@@ -1,9 +0,0 @@
-f=imread('split_img_6.jpg');
-figure,imhist(f)
-imshow(f)
-ylim('auto')
-g = histeq(f,256);%直方?均衡化
-figure , imshow(g)
-figure , imhist(g)%?示直方?
-ylim('auto')
-%imwrite(g,'C:/Users/samwu/Desktop/sample/equalization_018.jpg');
